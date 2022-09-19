60798f79ed9625e3e5c4e6ba1d1e329e8dd520bc
Rob Chiocchio <RobethX@users.noreply.github.com>
2022-09-18 21:03:36 -0400

    Create header

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <header className="Header">
+            <p>Edit <code>src/App.js</code> and save to reload.</p>
+        </header>
+    );
+}
