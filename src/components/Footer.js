0981aff0cbc55b91f6de95ca952e48216fe6de68
Rob Chiocchio <RobethX@users.noreply.github.com>
2022-09-18 20:39:52 -0400

    Update Footer.js
    Add links, additional attribution. Still a WIP

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,15 +13,15 @@ export default function Footer() {
             </div>
 
             <div className="Footer-row">
-                Morocco region and province map data from <a href="https://github.com/yousfiSaad/morocco-map">yousfiSaad/morocco-map</a>.
+                Morocco region and province map data from <a href="https://github.com/yousfiSaad/morocco-map">yousfiSaad/morocco-map</a>
             </div>
 
             <div className="Footer-credits Footer-row">
-                Made with <span role="img" aria-label="love">❤️</span> by <a href="">Rob Chiocchio</a>
+                Made with <span role="img" aria-label="love">❤️</span> by <a href="https://robch.io">Rob Chiocchio</a>
             </div>
 
             <div className="Footer-license Footer-row">
-                This project is licensed under <a href="">GNU General Public License v3.0</a>
+                This project is licensed under <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU General Public License v3.0</a>
             </div>
         </footer>
     );
