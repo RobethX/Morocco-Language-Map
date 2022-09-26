6633458333a65dca1961cf1518cd356d5b41aacd
Rob Chiocchio <RobethX@users.noreply.github.com>
2022-09-26 18:58:33 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ export default function Header() {
     return (
         <header className="Header">
             <h1>The Languages of Morocco</h1>
+            <i>And the disputed Western Sahara Region</i>
             <p>Edit <code>src/App.js</code> and save to reload.</p>
         </header>
     );
