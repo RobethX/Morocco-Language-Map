3329cfc719f1a4c3372bf8d2510af81501daeeef
Rob Chiocchio <RobethX@users.noreply.github.com>
2022-09-26 18:37:33 -0400

    Add title

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import "./Header.css";
 export default function Header() {
     return (
         <header className="Header">
+            <h1>The Languages of Morocco</h1>
             <p>Edit <code>src/App.js</code> and save to reload.</p>
         </header>
     );
